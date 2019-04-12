2d0a6fc2891d01d21af30fab2c2675c9c664f70d
陈帅 <chen_shuai@sinosoft.com.cn>
2019-04-12 16:07:26 +0800

    first commit

---
+++ app/src/main/java/com/mrchen/simpleh5andorid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mrchen.simpleh5andorid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
